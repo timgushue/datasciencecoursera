227ce6f1612ac5f90757d3a35e46a0e111fa7ccc
timgushue <gushue.tim@gmail.com>
2014-06-19 22:49:59 -0700

    Finishing best.R

--- ProgrammingAssignment3/best.R
+++ ProgrammingAssignment3/best.R
@@ -5,10 +5,21 @@
 best <- function(state, outcome) {
 ## Read outcome data
 raw.data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
-sub.set <- raw.data[,c(2, 7, 11, 17, 23)]
-names(sub.set)[c(1, 2, 3, 4, 5)] <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+pruned.data <- raw.data[,c(2, 7, 11, 17, 23)]
+names(pruned.data)[c(1, 2, 3, 4, 5)] <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
 
 ## Check that state and outcome are valid
+clean.state <- toupper(state)
+outcome <- tolower(outcome)
+if (!(clean.state %in% state.abb)) stop("invalid state")
+if (!(outcome %in% names(pruned.data)[c(3, 4, 5)])) stop("invalid outcome")
 
 ## Return hospital name in that state with lowest 30-day death rate
+state.data <- subset(pruned.data, pruned.data["state"] == clean.state)
+pruned.states <- subset(state.data, state.data[outcome] != "Not Available")
+min.value <- min(sapply(pruned.states[outcome],as.numeric))
+pos.values <- sapply(pruned.states[outcome], as.numeric)
+min.matrix <- pos.values <= min.value
+result <- subset(pruned.states, min.matrix)
+return(result["hospital"][[1]])
 }
