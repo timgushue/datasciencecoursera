1a1263c28e8b51aa72b3373437daa2ec73482264
timgushue <gushue.tim@gmail.com>
2014-06-22 10:38:38 -0700

    Adding rankhospital function

--- ProgrammingAssignment3/best.R
+++ ProgrammingAssignment3/best.R
@@ -3,23 +3,23 @@
 ## (i.e. lowest) 30-day mortality for the speciﬁed outcome in that state.
 
 best <- function(state, outcome) {
-## Read outcome data
-raw.data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
-pruned.data <- raw.data[,c(2, 7, 11, 17, 23)]
-names(pruned.data)[c(1, 2, 3, 4, 5)] <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+        ## Read outcome data
+        raw.data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
+        pruned.data <- raw.data[,c(2, 7, 11, 17, 23)]
+        names(pruned.data)[c(1, 2, 3, 4, 5)] <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
 
-## Check that state and outcome are valid
-clean.state <- toupper(state)
-outcome <- tolower(outcome)
-if (!(clean.state %in% state.abb)) stop("invalid state")
-if (!(outcome %in% names(pruned.data)[c(3, 4, 5)])) stop("invalid outcome")
+        ## Check that state and outcome are valid
+        clean.state <- toupper(state)
+        outcome <- tolower(outcome)
+        if (!(clean.state %in% state.abb)) stop("invalid state")
+        if (!(outcome %in% names(pruned.data)[c(3, 4, 5)])) stop("invalid outcome")
 
-## Return hospital name in that state with lowest 30-day death rate
-state.data <- subset(pruned.data, pruned.data["state"] == clean.state)
-pruned.states <- subset(state.data, state.data[outcome] != "Not Available")
-min.value <- min(sapply(pruned.states[outcome],as.numeric))
-pos.values <- sapply(pruned.states[outcome], as.numeric)
-min.matrix <- pos.values <= min.value
-result <- subset(pruned.states, min.matrix)
-return(result["hospital"][[1]])
+        ## Return hospital name in that state with lowest 30-day death rate
+        state.data <- subset(pruned.data, pruned.data["state"] == clean.state)
+        pruned.states <- subset(state.data, state.data[outcome] != "Not Available")
+        min.value <- min(sapply(pruned.states[outcome],as.numeric))
+        pos.values <- sapply(pruned.states[outcome], as.numeric)
+        min.matrix <- pos.values <= min.value
+        result <- subset(pruned.states, min.matrix)
+        return(result["hospital"][[1]])
 }
--- ProgrammingAssignment3/rankhospital.R
+++ ProgrammingAssignment3/rankhospital.R
@@ -1,3 +1,35 @@
 ## This function reads the outcome-of-care-measures.csv ﬁle and returns a 
 ## character vector with the name of the hospital that has the ranking 
 ## speciﬁed by the num argument
+
+rankhospital <- function(state, outcome, num = "best") {
+        ## Read outcome data
+        raw.data <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
+        pruned.data <- raw.data[,c(2, 7, 11, 17, 23)]
+        names(pruned.data)[c(1, 2, 3, 4, 5)] <- c("hospital", "state", "heart attack", "heart failure", "pneumonia")
+
+        ## Check that state and outcome are valid
+        clean.state <- toupper(state)
+        outcome <- tolower(outcome)
+        if (!(clean.state %in% state.abb)) stop("invalid state")
+        if (!(outcome %in% names(pruned.data)[c(3, 4, 5)])) stop("invalid outcome")
+
+        ## Sort rows by outcome data after removing missing entries
+        state.data <- subset(pruned.data, pruned.data["state"] == clean.state)
+        pruned.states <- subset(state.data, state.data[outcome] != "Not Available")
+        pruned.states[outcome] <- sapply(pruned.states[outcome], as.numeric)
+        ordered <- pruned.states[order(pruned.states[outcome], pruned.states["hospital"]),]
+
+        ## Store results in a data frame (subset was causing column name problems)
+        name <- as.vector(ordered[["hospital"]])
+        rate <- as.vector(ordered[[outcome]])
+        rank <- seq(dim(ordered)[1])
+        result <- data.frame("Hospital Name"=name, "Rate"=rate, "Rank"=rank)
+
+        ## Return hospital name in that state with the given rank 30-day death rate
+        if (num == "best") return(as.vector(head(result,1)[[1]]))
+        else if (num == "worst") return(as.vector(tail(result,1)[[1]]))
+        else if (as.numeric(num) > length(rank)) return(as.vector(NA))
+        else return(as.vector(result[num,1][1]))
+
+}
